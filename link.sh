c4049d5d2cc1b9ec9db97561ac85861d6c07a7a6
rodion890 <ttraxxasua@gmail.com>
2019-07-26 12:33:07 +0100

    Add very basic link script

---
+++ link.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# assumes dotfiles are always at ~/git/dotfiles
+
+# zsh
+ln -s ~/git/dotfiles/zsh ~/.zsh
+ln -s ~/git/dotfiles/zsh/zshrc ~/.zshrc
+
+# git
+ln -s ~/git/dotfiles/git/gitignore_global ~/.gitignore_global
+ln -s ~/git/dotfiles/git/gitconfig ~/.gitconfig
+
+# Brewfile
+ln -s ~/git/dotfiles/Brewfile ~/Brewfile
