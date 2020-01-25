bc679fb2ca492a9e58037b0a75826740612614c0
rodion890 <ttraxxasua@gmail.com>
2020-01-25 18:37:38 +0000

    Tidy zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,18 +6,6 @@ alias ll='ls -ahl'
 # clear
 alias clr='clear'
 
-# git
-alias gfp='git push --force-with-lease'
-alias gap='git add -p'
-alias gc='git commit'
-alias gs='git status'
-alias gp="git pull"
-alias gl="git log --pretty=oneline --topo-order --graph --abbrev-commit --decorate"
-
-# docker
-alias dsa='docker container stop $(docker ps -aq)'
-alias dl='docker container ls'
-
 ## bundler
 alias be='bundle exec'
 alias bi='bundle install'
